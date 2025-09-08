355171a51db96bced2b10bf7d7a2307a3f66ceb2
dhrubo-10 <sdhrubo770@gmail.com>
2025-09-08 22:21:34 +0600

    Bootstrap kernel, remap PIC, enter protected-mode

---
+++ UNIX/boot/h.s
@@ -0,0 +1,121 @@
+/* 32-bit startup: sets up IDT, GDT, paging, stack, and transitions to kernel main.
+ */
+
+.text
+.globl _idt,_gdt,_pg_dir
+_pg_dir:
+startup_32:
+	movl $0x10,%eax
+	mov %ax,%ds
+	mov %ax,%es
+	mov %ax,%fs
+	mov %ax,%gs
+	lss _stack_start,%esp
+	call setup_idt
+	call setup_gdt
+	movl $0x10,%eax
+	mov %ax,%ds
+	mov %ax,%es
+	mov %ax,%fs
+	mov %ax,%gs
+	lss _stack_start,%esp
+	xorl %eax,%eax
+1:	incl %eax
+	movl %eax,0x000000
+	cmpl %eax,0x100000
+	je 1b
+	movl %cr0,%eax
+	andl $0x80000011,%eax
+	testl $0x10,%eax
+	jne 1f
+	orl $4,%eax
+1:	movl %eax,%cr0
+	jmp after_page_tables
+
+setup_idt:
+	lea ignore_int,%edx
+	movl $0x00080000,%eax
+	movw %dx,%ax
+	movw $0x8E00,%dx
+
+	lea _idt,%edi
+	mov $256,%ecx
+rp_sidt:
+	movl %eax,(%edi)
+	movl %edx,4(%edi)
+	addl $8,%edi
+	dec %ecx
+	jne rp_sidt
+	lidt idt_descr
+	ret
+
+setup_gdt:
+	lgdt gdt_descr
+	ret
+
+.org 0x1000
+pg0:
+
+.org 0x2000
+pg1:
+
+.org 0x3000
+pg2:
+
+.org 0x4000
+after_page_tables:
+	pushl $0
+	pushl $0
+	pushl $0
+	pushl $L6
+	pushl $_main
+	jmp setup_paging
+L6:
+	jmp L6
+
+.align 2
+ignore_int:
+	incb 0xb8000+160
+	movb $2,0xb8000+161
+	iret
+
+.align 2
+setup_paging:
+	movl $1024*3,%ecx
+	xorl %eax,%eax
+	xorl %edi,%edi
+	cld;rep;stosl
+	movl $pg0+7,_pg_dir
+	movl $pg1+7,_pg_dir+4
+	movl $pg1+4092,%edi
+	movl $0x7ff007,%eax
+	std
+1:	stosl
+	subl $0x1000,%eax
+	jge 1b
+	xorl %eax,%eax
+	movl %eax,%cr3
+	movl %cr0,%eax
+	orl $0x80000000,%eax
+	movl %eax,%cr0
+	ret
+
+.align 2
+.word 0
+idt_descr:
+	.word 256*8-1
+	.long _idt
+.align 2
+.word 0
+gdt_descr:
+	.word 256*8-1
+	.long _gdt
+
+	.align 3
+_idt:	.fill 256,8,0
+
+_gdt:	.quad 0x0000000000000000
+	.quad 0x00c09a00000007ff
+	.quad 0x00c09200000007ff
+	.quad 0x0000000000000000
+	.fill 252,8,0

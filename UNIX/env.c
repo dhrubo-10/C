382f54b1c1e736e9755feeeeb8ff8434853b8866
dhrubo-10 <sdhrubo770@gmail.com>
2025-09-08 14:05:54 +0600

    Refactor parser: implement keyword table, symbol resolution, and expression tree builder

--- UNIX/env.c
+++ UNIX/env.c
@@ -280,4 +280,329 @@ loop:
 	}
 	return(ctab[c]);
 }
+
+subseq(c,a,b)
+{
+	if (spnextchar() != c)
+		return(a);
+	peekc = 0;
+	return(b);
+}
+
+
+getstr()
+{
+	register int c;
+	register char *sp;
+
+	nchstr = 1;
+	sp = savstr;
+	while((c=mapch('"')) >= 0) {
+		nchstr++;
+		if (sp >= &savstr[STRSIZ]) {
+			sp = savstr;
+			error("String too long");
+		}
+		*sp++ = c;
+	}
+	strptr = sp;
+	cval = isn++;
+	return(STRING);
+}
+
+
+putstr(lab)
+{
+	register char *sp;
+
+	if (lab) {
+		strflg++;
+		outcode("BNB", LABEL, lab, BDATA);
+	} else
+		outcode("B", BDATA);
+	for (sp = savstr; sp<strptr; )
+		outcode("1N", *sp++ & 0377);
+	outcode("100");
+	strflg = 0;
+}
+
+getcc()
+{
+	register int c, cc;
+	register char *ccp;
+
+	cval = 0;
+	ccp = &cval;
+	cc = 0;
+	while((c=mapch('\'')) >= 0)
+		if(cc++ < NCPW)
+			*ccp++ = c;
+	if(cc>NCPW)
+		error("Long character constant");
+	return(CON);
+}
+
+mapch(ac)
+{
+	register int a, c, n;
+	static mpeek;
+
+	c = ac;
+	if (a = mpeek)
+		mpeek = 0;
+	else
+		a = getchar();
+loop:
+	if (a==c)
+		return(-1);
+	switch(a) {
+
+	case '\n':
+	case '\0':
+		error("Nonterminated string");
+		peekc = a;
+		return(-1);
+
+	case '\\':
+		switch (a=getchar()) {
+
+		case 't':
+			return('\t');
+
+		case 'n':
+			return('\n');
+
+		case 'b':
+			return('\b');
+
+		case '0': case '1': case '2': case '3':
+		case '4': case '5': case '6': case '7':
+			n = 0;
+			c = 0;
+			while (++c<=3 && '0'<=a && a<='7') {
+				n =<< 3;
+				n =+ a-'0';
+				a = getchar();
+			}
+			mpeek = a;
+			return(n);
+
+		case 'r':
+			return('\r');
+
+		case '\n':
+			if (!inhdr)
+				line++;
+			inhdr = 0;
+			a = getchar();
+			goto loop;
+		}
+	}
+	return(a);
+}
+
+tree()
+{
+#define	SEOF	200
+#define	SSIZE	20
+	int *op, opst[SSIZE], *pp, prst[SSIZE];
+	register int andflg, o;
+	register struct hshtab *cs;
+	int p, ps, os;
+
+	space = treespace;
+	op = opst;
+	pp = prst;
+	cp = cmst;
+	*op = SEOF;
+	*pp = 06;
+	andflg = 0;
+
+advanc:
+	switch (o=symbol()) {
+
+	case NAME:
+		cs = csym;
+		if (cs->hclass==0 && cs->htype==0)
+			if(nextchar()=='(') {
+				/* set function */
+				cs->hclass = EXTERN;
+				cs->htype = FUNC;
+			} else if (initflg)
+				cs->hclass = EXTERN;
+			else {
+				/* set label */
+				cs->htype = ARRAY;
+				if (cs->hoffset==0)
+					cs->hoffset = isn++;
+			}
+		*cp++ = copname(cs);
+		goto tand;
+
+	case FCON:
+		if (!initflg)
+			outcode("BBNB1N1N1N1N0B", DATA,LABEL,
+			    cval, WDATA, fcval, PROG);
+
+	case CON:
+	case SFCON:
+		*cp++ = block(1,o,(o==CON?INT:DOUBLE),0,cval);
+		goto tand;
+
+	/* fake a static char array */
+	case STRING:
+		putstr(cval);
+		*cp++ = block(3, NAME, ARRAY+CHAR,0,STATIC,0,cval);
+
+tand:
+		if(cp>=cmst+cmsiz) {
+			error("Expression overflow");
+			exit(1);
+		}
+		if (andflg)
+			goto syntax;
+		andflg = 1;
+		goto advanc;
+
+	case INCBEF:
+	case DECBEF:
+		if (andflg)
+			o =+ 2;
+		goto oponst;
+
+	case COMPL:
+	case EXCLA:
+	case SIZEOF:
+		if (andflg)
+			goto syntax;
+		goto oponst;
+
+	case MINUS:
+		if (!andflg)  {
+			if ((peeksym=symbol())==FCON) {
+				fcval = - fcval;
+				goto advanc;
+			}
+			if (peeksym==SFCON) {
+				fcval = - fcval;
+				cval =^ 0100000;
+				goto advanc;
+			}
+			o = NEG;
+		}
+		andflg = 0;
+		goto oponst;
+
+	case AND:
+	case TIMES:
+		if (andflg)
+			andflg = 0; else
+			if(o==AND)
+				o = AMPER;
+			else
+				o = STAR;
+		goto oponst;
+
+	case LPARN:
+		if (andflg) {
+			o = symbol();
+			if (o==RPARN)
+				o = MCALL;
+			else {
+				peeksym = o;
+				o = CALL;
+				andflg = 0;
+			}
+		}
+		goto oponst;
+
+	case RBRACK:
+	case RPARN:
+		if (!andflg)
+			goto syntax;
+		goto oponst;
+
+	case DOT:
+	case ARROW:
+		mosflg++;
+		break;
+
+	}
+	/* binaries */
+	if (!andflg)
+		goto syntax;
+	andflg = 0;
+
+oponst:
+	p = (opdope[o]>>9) & 077;
+	if ((o==COMMA || o==COLON) && initflg)
+		p = 05;
+opon1:
+	ps = *pp;
+	if (p>ps || p==ps && (opdope[o]&RASSOC)!=0) {
+		switch (o) {
+
+		case INCAFT:
+		case DECAFT:
+			p = 37;
+			break;
+		case LPARN:
+		case LBRACK:
+		case CALL:
+			p = 04;
+		}
+		if (op >= &opst[SSIZE-1]) {
+			error("expression overflow");
+			exit(1);
+		}
+		*++op = o;
+		*++pp = p;
+		goto advanc;
+	}
+	--pp;
+	switch (os = *op--) {
+
+	case SEOF:
+		peeksym = o;
+		build(0);		/* flush conversions */
+		return(*--cp);
+
+	case CALL:
+		if (o!=RPARN)
+			goto syntax;
+		build(os);
+		goto advanc;
+
+	case MCALL:
+		*cp++ = block(0,0,0,0);	/* 0 arg call */
+		os = CALL;
+		break;
+
+	case INCBEF:
+	case INCAFT:
+	case DECBEF:
+	case DECAFT:
+		*cp++ = block(1, CON, INT, 0, 1);
+		break;
+
+	case LPARN:
+		if (o!=RPARN)
+			goto syntax;
+		goto advanc;
+
+	case LBRACK:
+		if (o!=RBRACK)
+			goto syntax;
+		build(LBRACK);
+		goto advanc;
+	}
+	build(os);
+	goto opon1;
+
+syntax:
+	error("Expression syntax");
+	errflush(o);
+	return(0);
+}
+
 /* section op */

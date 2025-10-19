27baecaed63b736cc80f9fb01618b965b4be4c4f
dhrubo-10 <sdhrubo770@gmail.com>
2025-10-19 19:00:09 +0600

    fix inode handling and writeback logic for consistency and stability

--- OS_0.01/fs_sec0/i_n.c
+++ OS_0.01/fs_sec0/i_n.c
@@ -4,8 +4,11 @@
 #include <linux/buffer_head.h>
 #include <linux/slab.h>
 #include <linux/time.h>
+#include <linux/fs.h>
+#include <linux/fs_context.h>
+#include <linux/exportfs.h>
 
-static struct inode *simplefs_iget(struct super_block *sb, unsigned long ino)
+static struct inode *simplefs_iget_internal(struct super_block *sb, unsigned long ino)
 {
 	struct inode *inode;
 	struct simplefs_inode *din;
@@ -17,34 +20,42 @@ static struct inode *simplefs_iget(struct super_block *sb, unsigned long ino)
 	unsigned long offset = (ino % per_block) * sizeof(struct simplefs_inode);
 
 	inode = new_inode(sb);
-	if (!inode) return NULL;
+	if (!inode)
+		return ERR_PTR(-ENOMEM);
 
 	bh = sb_bread(sb, block);
 	if (!bh) {
 		iput(inode);
-		return NULL;
+		return ERR_PTR(-EIO);
 	}
+
 	din = (struct simplefs_inode *)(bh->b_data + offset);
 
 	inode->i_ino = ino;
 	inode_init_owner(&init_user_ns, inode, NULL, le16_to_cpu(din->i_mode));
 	inode->i_mode = le16_to_cpu(din->i_mode);
-	inode->i_uid.val = le16_to_cpu(din->i_uid);
+	i_uid_write(inode, le16_to_cpu(din->i_uid));
+	i_gid_write(inode, 0); /* optional: extend structure for gid */
 	inode->i_size = le32_to_cpu(din->i_size);
 	inode->i_atime.tv_sec = le32_to_cpu(din->i_atime);
 	inode->i_mtime.tv_sec = le32_to_cpu(din->i_mtime);
 	inode->i_ctime.tv_sec = le32_to_cpu(din->i_ctime);
 
 	SIMPLEFS_I(inode)->i_blocks = le32_to_cpu(din->i_blocks);
-	for (int i = 0; i < 12; i++)
+	for (int i = 0; i < SIMPLEFS_N_BLOCKS; i++)
 		SIMPLEFS_I(inode)->i_block_ptrs[i] = le32_to_cpu(din->i_block[i]);
 
 	if (S_ISDIR(inode->i_mode)) {
+		inode->i_op = &simple_dir_inode_operations;
 		inode->i_fop = &simple_dir_operations;
-		inode->i_op  = &simple_dir_inode_operations;
-	} else {
+	} else if (S_ISREG(inode->i_mode)) {
+		inode->i_op = &simple_file_inode_operations;
 		inode->i_fop = &simplefs_file_ops;
-		inode->i_op  = &simple_file_inode_operations;
+	} else if (S_ISLNK(inode->i_mode)) {
+		inode->i_op = &page_symlink_inode_operations;
+		inode->i_mapping->a_ops = &simple_aops;
+	} else {
+		init_special_inode(inode, inode->i_mode, 0);
 	}
 
 	brelse(bh);
@@ -65,34 +76,38 @@ int simplefs_write_inode(struct inode *inode, struct writeback_control *wbc)
 
 	memset(&din, 0, sizeof(din));
 	din.i_mode = cpu_to_le16(inode->i_mode);
-	din.i_uid  = cpu_to_le16((u16)inode->i_uid.val);
-	din.i_size = cpu_to_le32((u32)inode->i_size);
+	din.i_uid  = cpu_to_le16(from_kuid(&init_user_ns, inode->i_uid));
+	din.i_size = cpu_to_le32(inode->i_size);
 	din.i_atime = cpu_to_le32((u32)inode->i_atime.tv_sec);
 	din.i_mtime = cpu_to_le32((u32)inode->i_mtime.tv_sec);
 	din.i_ctime = cpu_to_le32((u32)inode->i_ctime.tv_sec);
 	din.i_blocks = cpu_to_le32(SIMPLEFS_I(inode)->i_blocks);
-	for (int i = 0; i < 12; i++)
+
+	for (int i = 0; i < SIMPLEFS_N_BLOCKS; i++)
 		din.i_block[i] = cpu_to_le32(SIMPLEFS_I(inode)->i_block_ptrs[i]);
 
 	bh = sb_bread(sb, block);
-	if (!bh) return -EIO;
+	if (!bh)
+		return -EIO;
 
 	memcpy(bh->b_data + offset, &din, sizeof(din));
 	mark_buffer_dirty(bh);
+	sync_dirty_buffer(bh);
 	brelse(bh);
+
 	return 0;
 }
 
 static const struct inode_operations simple_file_inode_operations = {
-	.write_inode = simplefs_write_inode,
+	.setattr = simple_setattr,
 };
 
 struct inode *simplefs_iget_export(struct super_block *sb, unsigned long ino)
 {
-	return simplefs_iget(sb, ino);
+	return simplefs_iget_internal(sb, ino);
 }
 
 struct inode *simplefs_iget(struct super_block *sb, unsigned long ino)
 {
-	return simplefs_iget(sb, ino);
+	return simplefs_iget_internal(sb, ino);
 }

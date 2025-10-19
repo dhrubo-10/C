35b83f487f2a5618ffa963fbeb28533bcc611e02
dhrubo-10 <sdhrubo770@gmail.com>
2025-10-19 18:58:19 +0600

    fix and enhance superblock initialization and statfs handling

--- OS_0.01/fs_sec0/nm_i.c
+++ OS_0.01/fs_sec0/nm_i.c
@@ -8,7 +8,7 @@
 
 static const struct super_operations simplefs_super_ops;
 
-int simple_statfs(struct dentry *dentry, struct kstatfs *buf)
+int simplefs_statfs(struct dentry *dentry, struct kstatfs *buf)
 {
 	struct super_block *sb = dentry->d_sb;
 	struct simplefs_sb_info *sbi = SIMPLEFS_SB(sb);
@@ -16,10 +16,12 @@ int simple_statfs(struct dentry *dentry, struct kstatfs *buf)
 	buf->f_type = SIMPLEFS_MAGIC;
 	buf->f_bsize = sb->s_blocksize;
 	buf->f_blocks = sbi->s_blocks_count;
-	buf->f_bfree = 0; /* not tracked precisely here */
-	buf->f_bavail = 0;
+	buf->f_bfree = sbi->s_free_blocks;
+	buf->f_bavail = sbi->s_free_blocks;
 	buf->f_files = sbi->s_inodes_count;
-	buf->f_ffree = 0;
+	buf->f_ffree = sbi->s_free_inodes;
+	buf->f_namelen = SIMPLEFS_NAME_LEN;
+
 	return 0;
 }
 
@@ -31,22 +33,24 @@ int simplefs_fill_super(struct super_block *sb, void *data, int silent)
 	struct simplefs_sb_info *sbi;
 
 	sbi = kzalloc(sizeof(*sbi), GFP_KERNEL);
-	if (!sbi) return -ENOMEM;
+	if (!sbi)
+		return -ENOMEM;
 
 	sb->s_fs_info = sbi;
 	sb->s_blocksize = SIMPLEFS_BLOCK_SIZE;
-	sb->s_blocksize_bits = 10;
+	sb->s_blocksize_bits = ilog2(SIMPLEFS_BLOCK_SIZE);
+	sb->s_magic = SIMPLEFS_MAGIC;
+	sb->s_op = &simplefs_super_ops;
 
-	/* read superblock from block 0 */
-	bh = sb_bread(sb, 0);
+	bh = sb_bread(sb, SIMPLEFS_SUPERBLOCK_BLOCK_NUMBER);
 	if (!bh) {
 		kfree(sbi);
 		return -EIO;
 	}
+
 	ds = (struct simplefs_super *)bh->b_data;
 
 	if (le32_to_cpu(ds->s_magic) != SIMPLEFS_SUPER_MAGIC) {
-		pr_err("simplefs: bad magic\n");
 		brelse(bh);
 		kfree(sbi);
 		return -EINVAL;
@@ -55,19 +59,24 @@ int simplefs_fill_super(struct super_block *sb, void *data, int silent)
 	sbi->s_sbh = bh;
 	sbi->s_inodes_count = le32_to_cpu(ds->s_inodes_count);
 	sbi->s_blocks_count = le32_to_cpu(ds->s_blocks_count);
+	sbi->s_free_blocks = le32_to_cpu(ds->s_free_blocks);
+	sbi->s_free_inodes = le32_to_cpu(ds->s_free_inodes);
 	sbi->s_first_data_block = le32_to_cpu(ds->s_first_data_block);
 	sbi->s_itable_block = le32_to_cpu(ds->s_inode_table_block);
 	sbi->s_bmap_block = le32_to_cpu(ds->s_block_bitmap_block);
 
-	sb->s_magic = SIMPLEFS_MAGIC;
-	sb->s_op = &simplefs_super_ops;
-
-	root = simplefs_iget(sb, 0);
-	if (!root) return -EIO;
+	root = simplefs_iget(sb, SIMPLEFS_ROOT_INODE_NUMBER);
+	if (IS_ERR(root)) {
+		brelse(bh);
+		kfree(sbi);
+		return PTR_ERR(root);
+	}
 
 	sb->s_root = d_make_root(root);
 	if (!sb->s_root) {
 		iput(root);
+		brelse(bh);
+		kfree(sbi);
 		return -ENOMEM;
 	}
 
@@ -76,5 +85,5 @@ int simplefs_fill_super(struct super_block *sb, void *data, int silent)
 
 static const struct super_operations simplefs_super_ops = {
 	.drop_inode = generic_delete_inode,
-	.statfs = simple_statfs,
+	.statfs = simplefs_statfs,
 };
